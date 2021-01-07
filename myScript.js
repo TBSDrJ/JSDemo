57a085a4ccda16002643507c97bb17a11f114c1d
TBSDrJ <marcus.jaiclin@bishops.com>
2021-01-06 23:14:08 -0800

    Add a few basic things about checkboxes and radio buttons.

--- myScript.js
+++ myScript.js
@@ -2,6 +2,12 @@
 myDiv = document.getElementById("myDiv");
 console.log(myDiv);
 
+work = document.getElementById("work");
+console.log(work);
+
+spirit = document.getElementById("spirit");
+console.log(Object.getPrototypeOf(spirit));
+
 // Click event to attach to button
 function myClick () {
   // Quick check to verify that the function executes.
@@ -15,7 +21,21 @@ function myClick () {
   // Now, using += we are *appending* to the new contents of the div tag.
   myDiv.innerHTML += "\t\t<h1>Whoa.</h1>\n"
   // Notice mixing of quotation marks, just like in Python.
-  myDiv.innerHTML += "\t\t<img src='default.png' />\n";
-  // Notice here that we are appending the values of the variables.
-  myDiv.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
+  myDiv.innerHTML += "\t\t<img src='default.png' height=300/>\n";
+  if (work.checked) {
+    // Notice here that we are appending the values of the variables.
+    myDiv.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
+    myDiv.innerHTML += "<p>Box was checked.</p>\n";
+  } else {
+    myDiv.innerHTML += "\t\t<p>Box not checked.</p>\n";
+  }
+  if (spirit.checked) {
+    myDiv.innerHTML += "\t\t<p>Feeling the Spirit of Radio.</p>\n";
+  } else {
+    myDiv.innerHTML += "\t\t<p>Not Feeling the Spirit of Radio.</p>\n";
+  }
+}
+
+function boxClick() {
+  console.log(work.checked);
 }
